1eaefbe8e66d1c62546288948fe8a0d748de3623
Shayyan <hshayyan@gmail.com>
2025-04-06 13:04:50 -0400

    price changes

--- backend/utils/priceLogic.js
+++ backend/utils/priceLogic.js
@@ -2,18 +2,33 @@
 
 function calculateResalePrice(brand, category, originalPrice) {
     const categoryDiscounts = {
-    "tops": 0.6,         
-    "dresses": 0.5,     
-    "pants": 0.6,        
-    "skirts": 0.5,       
-    "outerwear": 0.8,     
-    "shoes": 0.7,        
-    "accessories": 0.9,  
-    "other": 0.4,         
+        "tops": 0.6,         
+        "dresses": 0.5,     
+        "pants": 0.6,        
+        "skirts": 0.5,       
+        "outerwear": 0.8,     
+        "shoes": 0.9,        
+        "accessories": 0.8,  
+        "other": 0.4,         
 };
 
-    const discount = categoryDiscounts[category.toLowerCase()] || 0.5;
-    return originalPrice * discount;
+    const brandMultipliers = {
+        "nike": 1.15,
+        "adidas": 1.0,
+        "gucci": 1.5,
+        "prada": 1.4,
+        "zara": 0.8,
+        "h&m": 0.7,
+        "puma": 0.7,
+        "louis vuitton": 1.6,
+        "supreme": 1.3,
+        "chanel": 1.7,
+        "other": 1.0
+};
+
+const discount = categoryDiscounts[category.toLowerCase()] || 0.5;
+const multiplier = brandMultipliers[brand.toLowerCase()] || brandMultipliers["other"];
+return originalPrice * discount * multiplier;
 }
 
 module.exports = { calculateResalePrice };
